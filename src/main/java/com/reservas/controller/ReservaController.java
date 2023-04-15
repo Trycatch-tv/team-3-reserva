f5bd75720495635823dc86ce242f8f2a80e7f16b
Andres Casas <ingfabiocasas@gmail.com>
2023-04-15 11:11:54 -0500

    Add files via upload

---
+++ src/main/java/com/reservas/controller/ReservaController.java
@@ -0,0 +1,104 @@
+package com.reservas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.reservas.model.Reserva;
+import com.reservas.service.ReservaService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/Reserva")
+public class ReservaController {
+
+	@Autowired
+	private ReservaService reservaService;
+
+	@GetMapping("/")
+	public ResponseEntity<List<Reserva>> listarReserva() {
+		return ResponseEntity.ok(reservaService.listarReservas());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Reserva> buscarReservaPorId(@PathVariable Integer id) {
+		try {
+			return ResponseEntity.ok(reservaService.buscarReservaPorId(id));
+		}
+
+		catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+
+	}
+
+	@PostMapping("/")
+	public ResponseEntity<Reserva> crearReserva(@RequestBody Reserva reserva) {
+		try {
+			Reserva ReservaCreada = reservaService.crearReserva(reserva);
+			return ResponseEntity.ok(ReservaCreada);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Reserva> editarReserva(@PathVariable Integer id, @RequestBody Reserva reserva) {
+		try {
+			Reserva reservaExistente = reservaService.buscarReservaPorId(id);
+			if (reserva.getMesa() != null && !reserva.getMesa().isEmpty())
+				reservaExistente.setMesa(reserva.getMesa());
+
+			if (reserva.getCliente() != null ) {
+				reservaExistente.setCliente(reserva.getCliente());
+			}
+			
+			if (reserva.getCantidadDePersonas() != null  && !reserva.getMesa().isEmpty() ) {
+				reservaExistente.setCantidadDePersonas(reserva.getCantidadDePersonas());
+			}
+			
+			if (reserva.getFecha() != null   ) {
+				reservaExistente.setFecha(reserva.getFecha());
+			}
+			
+			if (reserva.getHora() != null  ) {
+				reservaExistente.setHora(reserva.getHora());
+			}
+			
+			if (reserva.getComentario() != null  && !reserva.getComentario().isEmpty() ) {
+				reservaExistente.setComentario(reserva.getComentario());
+			}
+			
+			reservaService.actualizarReserva(reservaExistente);
+			
+
+			return ResponseEntity.ok(reservaExistente);
+
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Reserva> eliminarReserva(@PathVariable Integer id) {
+
+		try {
+			Reserva reservaEliminada = reservaService.buscarReservaPorId(id);
+			reservaService.eliminarReserva(id);
+			
+			return ResponseEntity.ok(reservaEliminada);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+}
