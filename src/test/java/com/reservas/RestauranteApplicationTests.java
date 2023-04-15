63f3b9c4979984216c545d5d597bf0023518161f
MMIT <ronaldgarciavazquez@gmail.com>
2023-04-15 10:31:32 -0400

    Rendirse nunca retroceder jamas 11

---
+++ src/test/java/com/reservas/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
