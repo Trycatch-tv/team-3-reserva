2e5573bd143bb73bedc96ded553a589a96e08af5
MMIT <ronaldgarciavazquez@gmail.com>
2023-04-19 09:37:23 -0400

    Reconfigurar para Render

--- src/main/java/com/reservas/config/SwaggerConfig.java
+++ src/main/java/com/reservas/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.reservas.controller"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
